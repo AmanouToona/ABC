cb413d0ed15c0383fb918a396813243c723b31bb
toona notepc <amanou.toona@gmail.com>
2020-08-30 16:11:18 +0900

    ABC177 end

---
+++ ABC177/A.py
@@ -0,0 +1,9 @@
+import sys
+import math
+
+D, T, S = map(int, sys.stdin.readline().strip().split())
+
+if D > T * S:
+    print('No')
+else:
+    print('Yes')
---
+++ ABC177/E.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+N = int(input())
+A = list(map(int, sys.stdin.readline().strip().split()))
+
+all_c = np.gcd(A[0], A[1])
+for i in range(2, len(A)):
+    all_c = np.gcd(all_c, A[i])
+
+

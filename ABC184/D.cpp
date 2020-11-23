ff3e3fc54539dd57106597051100a12a6737000a
toona notepc <amanou.toona@gmail.com>
2020-11-23 22:52:07 +0900

    solving

--- ABC184/D.cpp
+++ ABC184/D.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> f[100][100][100];
+
 int main() {
     int A, B, C;
     cin >> A >> B >> C;
 
-    
+    int draw = 1;
+
+    for (draw; draw < 100; draw++) {
+        if (A + draw < 100) {
+            f[A + draw][B][C] = 
+        }
+    }
+
+
 }

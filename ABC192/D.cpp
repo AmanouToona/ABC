71804d525fed04392945c2a9a5edda1dee38c577
toona notepc <amanou.toona@gmail.com>
2021-03-28 22:05:35 +0900

    review ABC197

--- ABC192/D.cpp
+++ ABC192/D.cpp
@@ -35,32 +35,21 @@ int main() {
         }
         return 0;
     }
-    ll n = max_num + 1; // n進数
-    while (true) {
-        if ((X[0] - '0') * pow(n, X.size() - 1) < M) {
-            ++n;
-        } else break;
-    }
 
-    int left = max_num + 1, right = n + 1;
+    ll left = max_num + 1, right = M + 1;
     if (check(left, X, M) == false) {
         cout << 0 << endl;
         return 0;
     }
 
     while (right - left > 1) {
-        int mid = (left + right) / 2;
+        ll mid = (left + right) / 2;
         if (check(mid, X, M)) left = mid;
         else right = mid;
     }
 
-    int ans = left;
-    // cout << "max" << max_num << endl;
-    // cout << "n" << n << endl;
-    // cout << "ans" << ans << endl;
-    
+    ll ans = left;  // int にして時間とかした
     ans = ans - max_num;
-    ans = max(ans, 0);
     cout << ans << endl;
     return 0;
 
--- ABC197/E.cpp
+++ ABC197/E.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 using ll = long long;
 
+
 int main() {
-    
+    int N;
+    cin >> N;
+
+    vector<vector<int>> color(N, vector<int>());
+
+    for (int i = 0; i < N; i++) {
+        int x, c;
+        cin >> x >> c;
+        color[c--].push_back(x);
+    }
+
+    vector<pair<int, int>> color_edge;
+    color_edge.push_back(make_pair(0, 0));
+    for (int i = 0; i < N; i++) {
+        if (color[i].empty()) continue;
+
+        sort(color[i].begin(), color[i].end());
+        color_edge.push_back(make_pair(color[i][0], color[i][color[i].size() - 1]));
+    }
+    color_edge.push_back(make_pair(0, 0));
+
+
+
+
 }

30c5d5e149c79fe1362147102d3457006f2af9ff
toona notepc <amanou.toona@gmail.com>
2021-08-09 00:34:13 +0900

    late AC: ABC213 D

--- ABC213/D.cpp
+++ ABC213/D.cpp
@@ -44,7 +44,7 @@ int main() {
         road[b].push_back(a);
     }
 
-    for (int i = 0; i < N - 1; i++) {
+    for (int i = 0; i < N; i++) {
         sort(road[i].begin(), road[i].end());
     }
 

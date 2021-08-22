0af6d6abfe21b819298772a84a0a3f19f327d58d
toona <amanou.toona@gmail.com>
2021-08-22 15:35:32 +0900

    late AC: ABC215 E

--- ABC215/E.cpp
+++ ABC215/E.cpp
@@ -22,38 +22,42 @@ int main() {
     string S;
     cin >> S;
 
-    vector<int> contest;
-    for (char s: S) {
-        if (s == 'A') {contest.push_back(0);}
-        else if (s == 'B') {contest.push_back(1);}
-        else if (s == 'C') {contest.push_back(2);}
-        else if (s == 'D') {contest.push_back(3);}
-        else if (s == 'E') {contest.push_back(4);}
-        else if (s == 'F') {contest.push_back(5);}
-        else if (s == 'G') {contest.push_back(6);}
-        else if (s == 'H') {contest.push_back(7);}
-        else if (s == 'I') {contest.push_back(8);}
-        else if (s == 'J') {contest.push_back(9);}
-    }
+    // n日目まで見た, 参加したコンテストの集合, 最後に参加したコンテスト
+    vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(1 << 10, vector<int>(11, 0)));
+    dp[0][0][10] = 1;
+    
+    // 初期化 は中で行う 配るdp
+    // i: i 日目まで見た, j: コンテストの出場実績の組みあわせ, k: 最後に出場したコンテスト
+    for (int i = 0; i < N; i++) {
+        int c = S[i] - 'A';
+        for (int j = 0; j < (1 << 10); j++) {
+            for (int k = 0; k < 11; k++) {
+                // constest c に出場しない
+                dp[i + 1][j][k] += dp[i][j][k];
+                dp[i + 1][j][k] %= mod;
 
-    vector<vector<vector<ll>>> dp(N, vector<vector<ll>>(10, vector<ll>(10, 0)));
-    dp[0][contest[0]][contest[0]] = 1;
-
-    for (int i = 1; i < N; i++) {
-        for (int j = 0; j < 10; j++) {
-            for (int k = 0; k < 10; k++) {
-                // contest i を選ぶ場合
-                dp[i][contest[j]][k]
+                // contest c に出場する
+                if (! ((j >> c) & 1)) {
+                    dp[i + 1][j | (1 << c)][c] += dp[i][j][k];
+                    dp[i + 1][j | (1 << c)][c] %= mod;
+                } else if (k == c) {
+                    dp[i + 1][j][c] += dp[i][j][k];
+                    dp[i + 1][j][c] %= mod;
+                }
             }
         }
     }
 
+
+
     int ans = 0;
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < (1 << 10); j++) {
         for (int k = 0; k < 10; k++) {
-            ans += dp[N - 1][j][k];
+            ans += dp[N][j][k];
             ans %= mod;
         }
     }
+
     cout << ans << endl;
+
 }

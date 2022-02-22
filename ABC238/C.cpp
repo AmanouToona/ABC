b56a6514da0b20b7d263b9f80aaa405c38f48935
toona <amanou.toona@gmail.com>
2022-02-23 08:26:10 +0900

    late sub: ABC238 C.

--- ABC238/C.cpp
+++ ABC238/C.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 using ll = long long;
 
+int MOD = 998244353;
+
+long long extGCD(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extGCD(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
+
+inline long long mod(long long a, long long m) { return (a % m + m) % m; }
+
+long long modinv(long long a, long long m) {
+    long long x, y;
+    extGCD(a, m, x, y);
+    return mod(x, m);
+}
+
 ll pow_ll(ll x, ll y) {
     ll ret = 1;
     while (y > 0) {
@@ -14,7 +35,29 @@ ll pow_ll(ll x, ll y) {
 
 int const INF = INT_MAX;
 
+ll sum(ll x) {
+    x %= MOD;
+    return (1 + x) % MOD * x % MOD * modinv(2, MOD) % MOD;
+}
 
 int main() {
+    ll N;
+    cin >> N;
 
+    ll inv2 = modinv(2, MOD);
+
+    ll ans = 0LL;
+    ll l = 1LL;
+    while (l <= N) {
+        // [l, r)
+        ll r = l * 10;
+        r = min<ll>(r, N + 1);
+
+        ll len = r - l;
+        ans += sum(len);
+        ans %= MOD;
+        l *= 10;
+    }
+
+    cout << ans << endl;
 }

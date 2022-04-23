fd9504a72be91bafb501be3ddb249eae684ccdae
toona <amanou.toona@gmail.com>
2022-04-23 13:18:37 +0900

    AC

--- ABC248/E.cpp
+++ ABC248/E.cpp
@@ -17,11 +17,11 @@ ll pow_ll(ll x, ll y) {
 int const INF = INT_MAX;
 string inf = "Infinity";
 
-bool colinear(pair<int, int> a, pair<int, int> b, pair<int, int> c) {
-    int x1 = b.first - a.first;
-    int x2 = c.first - a.first;
-    int y1 = b.second - a.second;
-    int y2 = c.second - a.second;
+bool colinear(pair<ll, ll> a, pair<ll, ll> b, pair<ll, ll> c) {
+    ll x1 = b.first - a.first;
+    ll x2 = c.first - a.first;
+    ll y1 = b.second - a.second;
+    ll y2 = c.second - a.second;
 
     return x1 * y2 - x2 * y1 == 0;
 }
@@ -30,9 +30,9 @@ int main() {
     int N, K;
     cin >> N >> K;
 
-    vector<pair<int, int>> P(N);
+    vector<pair<ll, ll>> P(N);
     for (int n = 0; n < N; n++) {
-        int X, Y;
+        ll X, Y;
         cin >> X >> Y;
         P[n] = make_pair(X, Y);
     }
@@ -48,20 +48,19 @@ int main() {
         for (int j = i + 1; j < N; j++) {
             if (!counted[i][j]) {
                 int count = 2;
-                counted[i][j] = true;
 
                 vector<int> l;
+                l.push_back(i);
+                l.push_back(j);
                 for (int k = j + 1; k < N; k++) {
                     if (colinear(P[i], P[j], P[k]) && !counted[j][k]) {
-                        counted[j][k] = true;
-                        counted[i][k] = true;
-                        count++;
                         l.push_back(k);
+                        count++;
                     }
                 }
                 for (int ii = 0; ii < l.size(); ii++) {
                     for (int jj = ii + 1; jj < l.size(); jj++) {
-                        counted[ii][jj] = true;
+                        counted[l[ii]][l[jj]] = true;
                     }
                 }
                 m[count]++;

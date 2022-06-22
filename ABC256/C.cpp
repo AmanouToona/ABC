c0eac9b100bdd68495c62e8e6dcf76dc54d85b4f
toona <amanou.toona@gmail.com>
2022-06-23 06:03:43 +0900

    Late sub: ABC256

--- ABC256/C.cpp
+++ ABC256/C.cpp
@@ -20,4 +20,31 @@ int main() {
     vector<int> h(3), w(3);
     for (int i = 0; i < 3; i++) cin >> h[i];
     for (int i = 0; i < 3; i++) cin >> w[i];
+
+    // |a|b|c|
+    // |d|e|f|
+    // |g|h|i|
+    int cnt = 0;
+    for (int a = 1; a <= w[0]; a++) {
+        for (int b = 1; a + b <= w[0]; b++) {
+            int c = w[0] - a - b;
+            if (c < 1) continue;
+
+            for (int d = 1; d <= w[1]; d++) {
+                for (int e = 1; e <= w[1]; e++) {
+                    int f = w[1] - d - e;
+                    if (f < 1) continue;
+
+                    int g = h[0] - a - d;
+                    int hh = h[1] - b - e;
+
+                    if (g < 1 || hh < 1) continue;
+
+                    if (w[2] - g - hh == h[2] - c - f && (w[2] - g - hh > 0))
+                        cnt++;
+                }
+            }
+        }
+    }
+    cout << cnt << endl;
 }
--- ABC256/E.cpp
+++ ABC256/E.cpp
@@ -21,5 +21,44 @@ int main() {
     cin >> N;
     vector<int> X(N), C(N);
     for (int i = 0; i < N; i++) cin >> X[i];
+    for (int i = 0; i < N; i++) X[i]--;
     for (int i = 0; i < N; i++) cin >> C[i];
+
+    vector<int> state(N, 0);
+    // 0: not visited
+    // 1: visited (calculating)
+    // 2: visited (calculated)
+    ll ans = 0;
+    for (int i = 0; i < N; i++) {
+        if (state[i] != 0) continue;
+
+        vector<int> vs;
+        auto dfs = [&](auto f, int u) {
+            state[u] = 1;
+
+            int v = X[u];
+            if (state[v] == 2) {
+                state[u] = 2;
+                return -1;
+            }
+            if (state[v] == 1) {
+                state[u] = 2;
+                vs.push_back(C[u]);
+                return v;
+            }
+
+            int loop_point = f(f, v);
+            state[u] = 2;
+            if (loop_point == -1) return -1;
+            vs.push_back(C[u]);
+            if (loop_point == u) return -1;
+            return loop_point;
+        };
+
+        dfs(dfs, i);
+
+        if (vs.size() == 0) continue;
+        ans += *min_element(vs.begin(), vs.end());
+    }
+    cout << ans << endl;
 }

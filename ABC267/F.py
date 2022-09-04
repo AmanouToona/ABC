aa393b3e3baf8b5ad260656d6ddc4704785aa407
toona <amanou.toona@gmail.com>
2022-09-05 08:11:04 +0900

    AC: ABC27 F change dfs -> bfs

--- ABC267/F.py
+++ ABC267/F.py
@@ -1,50 +1,44 @@
 import sys
+from collections import deque
 
 sys.setrecursionlimit(10 ** 8)
 
 
 def main():
+    input = sys.stdin.readline
     N = int(input())
 
     G = [[] for _ in range(N)]
     for _ in range(N - 1):
-        A, B = map(int, sys.stdin.readline().strip().split())
+        A, B = map(int, input().strip().split())
         A -= 1
         B -= 1
 
         G[A].append(B)
         G[B].append(A)
 
-    def furthest_point(u):
-        N = len(G)
+    def bfs(s):
         dist = [-1] * N
-
-        def dfs(u, p, d):
-            dist[u] = d
-
-            for v in G[u]:
-                if v == p:
+        que = deque([s])
+        dist[s] = 0
+        while que:
+            v = que.popleft()
+            d = dist[v]
+            for w in G[v]:
+                if dist[w] >= 0:
                     continue
-                dfs(v, u, d + 1)
-
-        dfs(u, -1, 0)
-
-        max_dist = -float("inf")
-        res = -1
-        for i, d in enumerate(dist):
-            if d > max_dist:
-                res = i
-                max_dist = d
-
-        return res
+                dist[w] = d + 1
+                que.append(w)
+        d = max(dist)
+        return dist.index(d)
 
     # 木の直径の両端A, Bを求める
-    A = furthest_point(0)
-    B = furthest_point(A)
+    A = bfs(0)
+    B = bfs(A)
 
     # ダブリングで祖先管理
     K = 0
-    while 2 ** K <= N:
+    while (1 << K) <= N:
         K += 1
 
     # A祖先
@@ -89,40 +83,31 @@ def main():
 
     Q = int(input())
     for _ in range(Q):
-        U, K = map(int, sys.stdin.readline().strip().split())
+        U, K = map(int, input().strip().split())
 
         U -= 1
 
-        # A を根とする木で Uからの距離が K である頂点を探索する
-        now = U
-        k = 0
-        while (1 << k) <= K:
-            if (1 << k) & K:
-                now = parentsa[k][now]
-                if now == -1:
-                    break
-            k += 1
+        k = []
+        count = 0
+        while 1 << count <= K:
+            if (1 << count) & K:
+                k.append(count)
+            count += 1
 
-        if now != -1:
-            print(now + 1)
-            continue
+        now_a = U
+        now_b = U
+        for _k in k:
+            if now_a != -1:
+                now_a = parentsa[_k][now_a]
+            if now_b != -1:
+                now_b = parentsb[_k][now_b]
 
-        # B を根とする木で Uからの距離が K である頂点を探索する
-        now = U
-        k = 0
-        while (1 << k) <= K:
-            if (1 << k) & K:
-                now = parentsb[k][now]
-                if now == -1:
-                    break
-
-            k += 1
-
-        if now != -1:
-            print(now + 1)
-            continue
-
-        print(now)
+        if now_a != -1:
+            print(now_a + 1)
+        elif now_b != -1:
+            print(now_b + 1)
+        else:
+            print(-1)
 
     return
 

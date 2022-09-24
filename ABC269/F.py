c5840ac986c096d6273547222fb3138c1e8b89eb
toona-laptop <amanou.toona@gmail.com>
2022-09-24 14:10:10 +0900

    Late sub: ABC269 F

--- ABC269/F.py
+++ ABC269/F.py
@@ -1,47 +1,26 @@
 import sys
 
-sys.setrecursionlimit(10 ** 8)
+sys.setrecursionlimit(10**8)
 
 MOD = 998244353
 
 
-def calc(x, y, m):
-    if y % 2 == 0:
-        y_even = y
-        y_odd = y - 1
-    else:
-        y_odd = y
-        y_even = y - 1
+def calc(row, col, m):
+    def calc_(lefttop, row, col):
+        ave = (lefttop + (lefttop + 2 * (col - 1) + 2 * m * (row - 1))) // 2
+        ave %= MOD
 
-    y1 = (1 + y_odd) * ((y + 1) // 2) // 2
-    y1 %= MOD
-    y2 = (2 + y_even) * (y // 2) // 2
-    y2 %= MOD
+        return ave * row * col
 
-    res = 0
+    res = calc_(1, (row + 1) // 2, (col + 1) // 2)
+    res += calc_(m + 2, row // 2, col // 2)
 
-    res += y1 * (x + 1) // 2
     res %= MOD
-    res += y2 * (x) // 2
-    res %= MOD
-
-    if x % 2 == 0:
-        x_even = x
-        x_odd = x - 1
-    else:
-        x_odd = x
-        x_even = x - 1
-
-    res += m * (2 + x_even) * ((x + 1) // 2) // 2 * ((y + 1) // 2)
-    res %= MOD
-    res += m * (1 + x_odd) * (x // 2) // 2 * (y // 2)
-    res %= MOD
-
     return res
 
 
 def main():
-    N, M = map(int, sys.stdin.readline().strip().split())
+    _, M = map(int, sys.stdin.readline().strip().split())
     Q = int(input())
 
     for _ in range(Q):
@@ -49,8 +28,8 @@ def main():
 
         ans = calc(b, d, M)
         ans -= calc(b, c - 1, M)
-        ans -= calc(a - 1, b, M)
-        ans += calc(a - 1, b - 1, M)
+        ans -= calc(a - 1, d, M)
+        ans += calc(a - 1, c - 1, M)
 
         ans %= MOD
         print(ans)

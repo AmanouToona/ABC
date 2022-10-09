2b2ce8a2e14ccc8506f295464c65d6ac0a796537
toona <amanou.toona@gmail.com>
2022-10-09 15:31:34 +0900

    Late sub: ABC272_E

--- ABC272/E.py
+++ ABC272/E.py
@@ -8,30 +8,37 @@ def main():
     N, M = map(int, sys.stdin.readline().strip().split())
     A = list(map(int, sys.stdin.readline().strip().split()))
 
-    final = A.copy()
-    for i in range(N):
-        final[i] += (i + 1) * M
-
-    zero = []
-    for i, a in enumerate(final):
-        t = ceil(max(-1, a) / (i + 1))
-        res = a - (t - 1) * (i + 1)
-
-        zero.append((t, res, i))
-
-    zero.sort()
-
-    ans = []
-    s = 0
-    for i in range(N):
-        if zero[s][0] > i:
-            ans.append[0]
+    check = [[] for _ in range(M)]
+    for i, a in enumerate(A):
+        if a > N:
             continue
 
-        if zero[s][1] - (zero[s][2] + 1) != 0:
-            ans.append[0]
-        while s + 1 < N and zero[s + 1][0] <= i:
-            s += 1
+        if a >= 0:
+            start = 0
+        else:
+            start = ceil(-a / (i + 1))
+
+        start = max(0, ceil(-a / (i + 1)) - 1)
+
+        end = ceil((N - a) / (i + 1))
+        end = min(end, M)
+
+        for j in range(start, end):
+            check[j].append(a + (i + 1) * (j + 1))
+
+    for c in check:
+        search = [False] * (len(c) + 1)
+
+        for c_ in c:
+            if c_ >= len(search):
+                continue
+            search[c_] = True
+
+        for i, s in enumerate(search):
+            if s:
+                continue
+            print(i)
+            break
 
     return
 

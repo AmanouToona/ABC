977a31254aab9e4ca04fff172798fccd24195ee0
toona-laptop <amanou.toona@gmail.com>
2022-10-22 23:43:45 +0900

    Late sub: ABC274

--- ABC274/E.py
+++ ABC274/E.py
@@ -12,30 +12,30 @@ def get_dist(point1, point2):
 def main():
     N, M = map(int, sys.stdin.readline().strip().split())
 
-    point = [(0, 0)]
+    points = []
     for _ in range(N):
         x, y = map(int, sys.stdin.readline().strip().split())
-        point.append((x, y))
+        points.append((x, y))
 
     for _ in range(M):
         p, q = map(int, sys.stdin.readline().strip().split())
-        point.append((p, q))
+        points.append((p, q))
 
-    dp = [[float("inf")] * (N + M + 1) for _ in range(1 << (N + M + 1))]
-    dp[0][N + M] = 0
+    dp = [[float("inf")] * (N + M) for _ in range(1 << (N + M))]
 
-    point.reverse()
+    points.reverse()
+    for i, point in enumerate(points):
+        dp[1 << i][i] = get_dist((0, 0), point)
 
-    ans = float("inf")
-    # 状態は 街 宝箱 の順番
-    for s in range(1 << (N + M + 1)):
-        for v in range(N + M + 1):
-            for u in range(N + M + 1):
+    # 状態は 原点 街 宝箱 の順番
+    for s in range(1 << (N + M)):
+        for v in range(N + M):
+            for u in range(N + M):
                 if u == v:
                     continue
-                if (s >> u) & 1 == 0 and s != 0:
-                    continue
                 if (s >> v) & 1:
+                    continue
+                if (s >> u) & 1 == 0 and s != 0:
                     continue
 
                 speed = 1
@@ -43,24 +43,29 @@ def main():
                     if (s >> i) & 1:
                         speed *= 2
 
-                dist = get_dist(point[u], point[v])
+                dist = get_dist(points[u], points[v])
                 time = dist / speed
 
-                if v == N + M:
-                    is_goal = True
-                else:
-                    is_goal = False
-                for i in range(N + M - 1, M - 1, -1):
-                    if not is_goal:
-                        break
-                    if not (s >> i) & 1:
-                        is_goal = False
-                        break
-
-                if is_goal:
-                    ans = min(ans, dp[s][u] + time)
-
                 dp[s | (1 << v)][v] = min(dp[s | (1 << v)][v], dp[s][u] + time)
+
+    goal = 0
+    for i in range(M, N + M):
+        goal += 1 << i
+
+    ans = float("inf")
+    for i in range(1 << M):
+        goal_ = goal + i
+
+        speed = 1
+        for i in range(M):
+            if (goal_ >> i) & 1:
+                speed *= 2
+
+        for j in range(len(points)):
+            if not (goal_ >> j) & 1:
+                continue
+
+            ans = min(ans, dp[goal_][j] + get_dist((0, 0), points[j]) / speed)
 
     print(ans)
 

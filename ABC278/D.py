6bd2d8afb3510ee88bbb7a2fedba83c633b8e688
toona <amanou.toona@gmail.com>
2022-11-20 00:16:52 +0900

    Refactor: ABC278 D

--- ABC278/D.py
+++ ABC278/D.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 sys.setrecursionlimit(10 ** 8)
 
@@ -9,31 +10,24 @@ def main():
     Q = int(input())
 
     base = 0
-    update_all = -1
-    addition = dict()
-    update_a = [-1] * len(A)
+    addition = defaultdict(int)
+
     for i, a in enumerate(A):
         addition[i] = a
 
-    for q in range(Q):
+    for _ in range(Q):
         query = list(map(int, sys.stdin.readline().strip().split()))
 
         if query[0] == 1:
             base = query[1]
-            update_all = q
+            addition = defaultdict(int)
         elif query[0] == 2:
             i = query[1] - 1
-            if update_a[i] < update_all:
-                addition[i] = query[2]
-            else:
-                addition[i] += query[2]
-            update_a[i] = q
+            x = query[2]
+            addition[i] += x
         else:
             i = query[1] - 1
-            if update_a[i] < update_all:
-                print(base)
-            else:
-                print(base + addition[i])
+            print(base + addition[i])
 
     return
 

9f853b8ac68297b202c24aa04f082173e9ec8ce9
toona <amanou.toona@gmail.com>
2022-11-20 15:26:05 +0900

    AC: ABC278 F

--- ABC278/F.py
+++ ABC278/F.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+
 sys.setrecursionlimit(10 ** 8)
 
 
@@ -7,37 +7,40 @@ def main():
     N = int(input())
 
     S = []
-    frm = defaultdict(list)
-    for n in range(N):
-        s = sys.stdin.readline().strip()
-        S.append(s)
+    for _ in range(N):
+        S.append(sys.stdin.readline().strip())
 
-        frm[ord(s[0]) - ord("a")].append(n)
-
-    # dp[state][now use] = -1 未着手 0 負け確 1 勝確
+    # dp[state][n] := n を消費して 状態 state であるとき先攻が勝つなら1 後攻が勝つなら 0
     dp = [[-1] * N for _ in range(1 << N)]
 
-    def dfs(u, state, first):
-        if dp[u][state] != -1:
-            return dp[u][state]
+    def win(u, state):
+        if dp[state][u] != -1:
+            return dp[state][u]
 
-        s = S[u]
-        f = ord(s[-1]) - ord("a")
+        can_win = []
+        for v in range(N):
+            if (state >> v) & 1:
+                continue
 
-        win = False
-        for v in frm[f]:
-            if dfs(v, state) == 1:
-                win = True
+            if S[u][-1] != S[v][0]:
+                continue
+            can_win.append(win(v, state=(state | 1 << v)))
 
-        if win:
-            dp[u][state] = 1
+        if not can_win:
+            dp[state][u] = 1
+        elif all(can_win):
+            dp[state][u] = 0
         else:
+            dp[state][u] = 1
 
-            dp[u][state] = -1
-        return dp[u][state]
+        return dp[state][u]
 
     for n in range(N):
+        if win(n, 1 << n):
+            print("First")
+            return
 
+    print("Second")
 
     return
 

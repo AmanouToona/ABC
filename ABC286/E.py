fee3f49eaf94a166ce8cecf7286d180ef6d5077e
toona <amanou.toona@gmail.com>
2023-02-05 12:33:30 +0900

    Late sub: ABC286 E

--- ABC286/E.py
+++ ABC286/E.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import product
 
 sys.setrecursionlimit(10 ** 8)
 
@@ -8,7 +9,8 @@ def main():
     A = list(map(int, sys.stdin.readline().strip().split()))
 
     # ワーシャルフロイド
-    ans = [[[float("inf"), 0]] * N for _ in range(N)]
+    cnt = [[N] * N for _ in range(N)]
+    val = [[0] * N for _ in range(N)]
 
     for n in range(N):
         S = sys.stdin.readline().strip()
@@ -16,37 +18,30 @@ def main():
         for i, s in enumerate(S):
             if s == "N":
                 continue
-            ans[n][i] = [1, A[n] + A[i]]
 
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                cost = ans[i][k][0] + ans[k][j][0]
-                gain = ans[i][k][1] + ans[k][j][1] - A[k]
+            cnt[n][i] = 1
+            val[n][i] = A[n] + A[i]
 
-                if cost > ans[i][j][0]:
-                    continue
+    for k, i, j in product(range(N), repeat=3):
+        _cnt = cnt[i][k] + cnt[k][j]
+        _val = val[i][k] + val[k][j] - A[k]
 
-                if cost < ans[i][j][0]:
-                    ans[i][j] = [cost, gain]
-                else:
-                    ans[i][j][1] = max(ans[i][j][1], gain)
+        if _cnt < cnt[i][j]:
+            cnt[i][j] = _cnt
+            val[i][j] = _val
+        elif _cnt == cnt[i][j] and val[i][j] < _val:
+            val[i][j] = _val
 
     Q = int(input())
     for _ in range(Q):
-        U, V = map(int, sys.stdin.readline().strip().split())
-        U -= 1
-        V -= 1
+        u, v = map(int, sys.stdin.readline().strip().split())
+        u -= 1
+        v -= 1
 
-        cost = ans[U][V][0]
-        gain = ans[U][V][1]
-
-        if cost == float("inf"):
+        if cnt[u][v] == N:
             print("Impossible")
         else:
-            print(f"{cost} {gain}")
-
-    return
+            print(f"{cnt[u][v]} {val[u][v]}")
 
 
 if __name__ == "__main__":

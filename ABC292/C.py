2934d6878ab8655b90a225e356451ad5ab85257f
toona <amanou.toona@gmail.com>
2023-03-16 21:03:16 +0900

    Refactor: ABC292_C

--- ABC292/C.py
+++ ABC292/C.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 
 sys.setrecursionlimit(10 ** 8)
 
@@ -7,18 +6,18 @@ sys.setrecursionlimit(10 ** 8)
 def main():
     N = int(input())
 
-    cnt = defaultdict(int)
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            if i * j > N:
-                break
-            cnt[i * j] += 1
+    cnt = [0] * (N + 1)
 
-    ans = 0
     for i in range(1, N + 1):
-        if cnt[i] == 0 or cnt[N - i] == 0:
-            continue
-        ans += cnt[i] * cnt[N - i]
+        for j in range(i, N + 1, i):
+            cnt[j] += 1
+
+    # ab = x, cd = y
+    ans = 0
+    for x in range(1, N + 1):
+        y = N - x
+
+        ans += cnt[x] * cnt[y]
 
     print(ans)
 

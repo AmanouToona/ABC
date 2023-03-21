bfa429317a96537a7a0b366d310ca308b003ec16
toona <amanou.toona@gmail.com>
2023-03-21 10:56:34 +0900

    Late sub

--- ABC293/E.py
+++ ABC293/E.py
@@ -6,6 +6,30 @@ sys.setrecursionlimit(10 ** 8)
 def main():
     A, X, M = map(int, sys.stdin.readline().strip().split())
 
+    global cache
+    cache = dict()
+
+    def f(x):
+        global cache
+
+        if x in cache.keys():
+            return cache[x]
+
+        if x == 0:
+            return 0
+
+        r = f(x // 2) + (f(x // 2) * pow(A, x // 2, M)) % M
+
+        if x % 2 == 1:
+            r = r * A + 1
+
+        r %= M
+
+        cache[x] = r
+        return r
+
+    print(f(X))
+
     return
 
 
